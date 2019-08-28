dae65f471493dd9da84579b6e4cec3345a34a73d
Lorenzo Meninato <lorenzomeninato@gmail.com>
2019-08-27 21:53:25 -0400

    Timing larger file

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 ./src/write_data.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+if [ "$1" = "-t" ]; then
+  echo "Timing report creation..."
+  time python3 ./src/write_data.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+else
+  python3 ./src/write_data.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+fi
