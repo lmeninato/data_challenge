63df121b0dbc32417509461f78877c0dda7bd392
Lorenzo Meninato <lorenzomeninato@gmail.com>
2019-09-02 17:01:08 -0400

    Equivalent sql command
    Tested on postgresql 10.0

---
+++ as_sql.sql
@@ -0,0 +1,24 @@
+select "Year", "Month", "Border", "Measure", "Value", 
+COALESCE("Average", 0)
+from
+(select "Year", "Month", "Border", "Measure", "Value",
+LAG("avg_value") OVER
+      (PARTITION BY "Measure", "Border" ORDER BY "Year", "Month")
+      as "Average"
+from
+(select *,
+ROUND(AVG("Value") OVER
+      (PARTITION BY "Measure", "Border" ORDER BY "Year", "Month"))
+       as avg_value
+from
+(select EXTRACT(YEAR FROM "Date") "Year",
+	   EXTRACT(MONTH FROM "Date") "Month",
+       "Border",
+       "Measure",
+       SUM("Value") AS "Value"
+from border_crossings
+group by "Year", "Month", "Border", "Measure")
+as grouped_crossings)
+as averaged_crossings)
+as lagged_average_crossings
+order by "Year" desc, "Month" desc, "Value" desc, "Border" desc, "Measure" desc;
